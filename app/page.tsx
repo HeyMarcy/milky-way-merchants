248f29a5de3dbb57a1957ed56da4c165508712db
Marcy Montross <marcy.montross@gmail.com>
2023-06-16 13:16:03 -0500

    fixes destructuring error

--- app/page.tsx
+++ app/page.tsx
@@ -20,17 +20,31 @@ const getAgent = async (): Promise<Agent> => {
 };
 
 export default async function Home() {
-  const data = await getAgent();
+  const getAgentDataResponse = await fetch(
+    "https://api.spacetraders.io/v2/my/agent",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.TOKEN}`,
+      },
+      next: { revalidate: 10 },
+    }
+  );
+
+  const { data } = await getAgentDataResponse.json();
+  const { accountId, symbol, credits, startingFaction } = data;
   console.log(data);
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
+        <p>{JSON.stringify(data, null, 3)}</p>
         <AgentCard
-          accountId={data.accountId ?? "Account ID"}
-          symbol={data.symbol}
-          headquarters={data.accountId}
-          credits={data.credits ?? 0}
-          startingFaction={data.startingFaction}
+          accountId={accountId ?? "Account ID"}
+          symbol={symbol}
+          headquarters={accountId}
+          credits={credits ?? 0}
+          startingFaction={startingFaction}
         />
       </div>
     </main>
